dd91d9a420aaf2d102b54c6fd569abb304d890b7
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-05-04 00:16:32 +0530

    updated client methods using utilities

--- pyclipdrop/client.py
+++ pyclipdrop/client.py
@@ -1,15 +1,14 @@
 import os
+import io
 import requests
 from typing import Text
 from pathlib import Path
 
 from pyclipdrop.settings import settings
-from pyclipdrop.utilities import FileUtilities, URLUtilities
+from pyclipdrop.utilities import InputUtilities, OutputUtilities
 
 
 # TODO: Add docstrings
-# TODO: Remove duplicate code
-# TODO: Suppport URL input
 class PyClipdropClient:
     def __init__(self, api_key: Text = None, base_url: Text = settings.BASE_URL, version: Text = settings.VERSION):
         self.api_key = api_key or os.environ.get('CLIPDROP_API_KEY')
@@ -20,13 +19,8 @@ class PyClipdropClient:
         self.version = version
 
     def text_to_image(self, prompt: Text, output_file: Text = 'output.png'):
-        # Check if the path to the file exists
-        if not FileUtilities.is_valid_parent_directory(output_file):
-            raise ValueError("The path to the output file does not exist.")
-
-        # Check if the output file has a .png extension
-        if FileUtilities.get_suffix_from_file_path(output_file) != '.png':
-            raise ValueError("Output file must be a .png file.")
+        # Check if the output file is valid
+        OutputUtilities(output_file, supported_extensions=['.png']).validate_output_file()
 
         response = requests.post(
             f'{self.base_url}/text-to-image/{self.version}',
@@ -45,45 +39,28 @@ class PyClipdropClient:
             response.raise_for_status()
 
     def replace_background(self, input_file: Text, prompt: Text, output_file: Text = None):
-        input_path = Path(input_file)
-        input_suffix = input_path.suffix
+        # get input data and suffix if the input file is valid
+        image_data, input_suffix = InputUtilities(input_file, supported_extensions=['.png', '.jpg', '.webp']).get_data_and_suffix()
 
         # If the output file is not specified, use 'output' with the same extension as the input file
         if not output_file:
             output_file = f'output{input_suffix}'
 
-        output_path = Path(output_file)
-        output_suffix = output_path.suffix
+        # Check if the output file is valid
+        OutputUtilities(output_file, supported_extensions=[input_suffix]).validate_output_file()
 
-        # Check if the input file exists
-        if not input_path.exists():
-            raise ValueError("The input file does not exist.")
-
-        # Check if the input file has a .png, .jpg or .webp extension
-        if input_suffix not in ['.png', '.jpg', '.webp']:
-            raise ValueError("Output file must be a .png, .jpg or .webp file.")
-        
-        # Check if the output file has the same extension as the input file
-        if output_suffix != input_suffix:
-            raise ValueError("Output file must have the same extension as the input file.")
-        
-        # Check if the path to the output file exists
-        if not output_path.parent.exists():
-            raise ValueError("The path to the output file does not exist.")
-
-        with open(input_file, 'rb') as image_file:
-            response = requests.post(
-                f'{self.base_url}/replace-background/{self.version}',
-                files={
-                    'image_file': (input_file, image_file, f'image/{input_suffix[1:]}')
-                },
-                data={
-                    'prompt': prompt
-                },
-                headers={
-                    'x-api-key': self.api_key
-                }
-            )
+        response = requests.post(
+            f'{self.base_url}/replace-background/{self.version}',
+            files={
+                'image_file': (input_file, io.BytesIO(image_data), f'image/{input_suffix[1:]}')
+            },
+            data={
+                'prompt': prompt
+            },
+            headers={
+                'x-api-key': self.api_key
+            }
+        )
 
         if (response.ok):
             with open(output_file, 'wb') as f:
@@ -92,38 +69,21 @@ class PyClipdropClient:
             response.raise_for_status()
         
     def remove_background(self, input_file: Text, output_file: Text = 'output.png'):
-        input_path = Path(input_file)
-        input_suffix = input_path.suffix
+        # get input data and suffix if the input file is valid
+        image_data, input_suffix = InputUtilities(input_file, supported_extensions=['.png', '.jpg', '.webp']).get_data_and_suffix()
 
-        output_path = Path(output_file)
-        output_suffix = output_path.suffix
+        # Check if the output file is valid
+        OutputUtilities(output_file, supported_extensions=['.png', '.jpg', '.webp']).validate_output_file()
 
-        # Check if the input file exists
-        if not input_path.exists():
-            raise ValueError("The input file does not exist.")
-
-        # Check if the input file has a .png, .jpg or .webp extension
-        if input_suffix not in ['.png', '.jpg', '.webp']:
-            raise ValueError("Output file must be a .png, .jpg or .webp file.")
-        
-        # Check if the output file has a .png, .jpg or .webp extension
-        if output_suffix not in ['.png', '.jpg', '.webp']:
-            raise ValueError("Output file must be a .png, .jpg or .webp file.")
-        
-        # Check if the path to the output file exists
-        if not output_path.parent.exists():
-            raise ValueError("The path to the output file does not exist.")
-
-        with open(input_file, 'rb') as image_file:
-            response = requests.post(
-                f'{self.base_url}/remove-background/{self.version}',
-                files={
-                    'image_file': (input_file, image_file, f'image/{input_suffix[1:]}')
-                },
-                headers={
-                    'x-api-key': self.api_key
-                }
-            )
+        response = requests.post(
+            f'{self.base_url}/remove-background/{self.version}',
+            files={
+                'image_file': (input_file, image_data, f'image/{input_suffix[1:]}')
+            },
+            headers={
+                'x-api-key': self.api_key
+            }
+        )
 
         if (response.ok):
             with open(output_file, 'wb') as f:
