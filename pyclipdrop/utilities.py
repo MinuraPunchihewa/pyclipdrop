30461e7a5d901864f4bfda684b24aecd3bfe7d2e
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-05-04 15:43:56 +0530

    removed the params from input and output utils and made methods static

--- pyclipdrop/utilities.py
+++ pyclipdrop/utilities.py
@@ -5,42 +5,36 @@ from typing import Text, List, Tuple
 
 
 class InputUtilities:
-    def __init__(self, input_file: Text, supported_extensions: List[Text] = None) -> None:
-        self.input_file = input_file
-        self.supported_extensions = supported_extensions
+    @staticmethod
+    def get_data_and_suffix(input_file: Text, supported_extensions: List[Text] = None) -> Tuple[bytes, Text]:
+        if FileUtilities.is_valid_file_path(input_file):
+            input_suffix = FileUtilities.get_suffix_from_file_path(input_file)
+            image_data = FileUtilities.get_data_from_file_path(input_file)
 
-    def get_data_and_suffix(self) -> Tuple[bytes, Text]:
-        if FileUtilities.is_valid_file_path(self.input_file):
-            input_suffix = FileUtilities.get_suffix_from_file_path(self.input_file)
-            image_data = FileUtilities.get_data_from_file_path(self.input_file)
-
-        elif URLUtilities.is_valid_url(self.input_file):
-            input_suffix = URLUtilities.get_suffix_from_url(self.input_file)
-            image_data = URLUtilities.get_data_from_url(self.input_file)
+        elif URLUtilities.is_valid_url(input_file):
+            input_suffix = URLUtilities.get_suffix_from_url(input_file)
+            image_data = URLUtilities.get_data_from_url(input_file)
 
         else:
             raise ValueError("Input file must be a valid file path or URL.")
 
-        if self.supported_extensions and input_suffix not in self.supported_extensions:
-            raise ValueError(f"Input file must have one of the following extensions: {', '.join(self.supported_extensions)}")
+        if supported_extensions and input_suffix not in supported_extensions:
+            raise ValueError(f"Input file must have one of the following extensions: {', '.join(supported_extensions)}")
 
         return image_data, input_suffix
     
 
 class OutputUtilities:
-    def __init__(self, output_file: Text, supported_extensions: List[Text] = None) -> None:
-        self.output_file = output_file
-        self.supported_extensions = supported_extensions
-
-    def validate_output_file(self) -> Text:
-        if FileUtilities.is_valid_parent_directory(self.output_file):
-            output_suffix = FileUtilities.get_suffix_from_file_path(self.output_file)
+    @staticmethod
+    def validate_output_file(output_file: Text, supported_extensions: List[Text] = None) -> Text:
+        if FileUtilities.is_valid_parent_directory(output_file):
+            output_suffix = FileUtilities.get_suffix_from_file_path(output_file)
 
         else:
             raise ValueError("The path to the output file does not exist.")
         
-        if self.supported_extensions and output_suffix not in self.supported_extensions:
-            raise ValueError(f"Output file must have one of the following extensions: {', '.join(self.supported_extensions)}")
+        if supported_extensions and output_suffix not in supported_extensions:
+            raise ValueError(f"Output file must have one of the following extensions: {', '.join(supported_extensions)}")
 
 
 class URLUtilities:
