7fa0f65c2cf8bc0db6c4e2a0afc6e36f5c29960a
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-06-03 00:05:36 +0530

    updated output handler by incorporating logic relevant to it

--- pyclipdrop/io_handlers/output_handler.py
+++ pyclipdrop/io_handlers/output_handler.py
@@ -1,19 +1,34 @@
+from pathlib import Path
 from typing import Text, List
-from pyclipdrop.utilities import FileUtilities
+from pyclipdrop.utilities import get_extension_from_file_path, is_extension_supported
+from pyclipdrop.exceptions import FilePathError, FileExtensionError, FileOpenError, FileWriteError
 
 
 class OutputHandler:
     def __init__(self, output_file: Text, supported_extensions: List[Text] = None) -> None:
         self.output_file = output_file
-        self.supported_extensions = supported_extensions + ['.jpeg'] if '.jpg' in supported_extensions else supported_extensions
+        self.supported_extensions = supported_extensions
         self.output_extension = None
 
     def validate(self) -> None:
-        if FileUtilities.is_valid_parent_directory(self.output_file):
-            self.output_extension = FileUtilities.get_suffix_from_file_path(self.output_file)
+        if self._is_valid_parent_directory():
+            self.output_extension = self.set_extension(get_extension_from_file_path(self.output_file))
 
         else:
-            raise ValueError("The path to the output file does not exist.")
+            raise FilePathError("The path to the output file does not exist.")
         
-        if self.supported_extensions and self.output_extension not in self.supported_extensions:
-            raise ValueError(f"Output file must have one of the following extensions: {', '.join(self.supported_extensions)}")
+        if not is_extension_supported(self.output_extension, self.supported_extensions):
+            raise FileExtensionError("The input file should be one of the supported extensions: " + str(self.supported_extensions))
+        
+    def _is_valid_parent_directory(self) -> bool:
+        return Path(self.output_file).parent.exists()
+        
+    def write(self, data: bytes) -> None:
+        try:
+            with open(self.output_file, 'wb') as f:
+                try:
+                    f.write(data)
+                except (IOError, OSError) as e:
+                    raise FileWriteError("Error writing to file: " + str(e))
+        except (FileNotFoundError, PermissionError, OSError) as e:
+            raise FileOpenError("Error opening file: " + str(e))
