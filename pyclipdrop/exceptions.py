9f83a69918b7e0aec9ec56ccefc19e595da10245
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-06-02 11:47:22 +0530

    created custom exceptions for opening and writing to file

--- pyclipdrop/exceptions.py
+++ pyclipdrop/exceptions.py
@@ -4,3 +4,17 @@ class APIRequestError(Exception):
     Exception raised for errors in the API request to Clipdrop.
     """
     pass
+
+
+class FileOpenError(Exception):
+    """
+    Exception raised for errors in opening a file.
+    """
+    pass
+
+
+class FileWriteError(Exception):
+    """
+    Exception raised for errors in writing to a file.
+    """
+    pass
