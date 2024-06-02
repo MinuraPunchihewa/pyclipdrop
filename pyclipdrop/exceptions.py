aef3d5e9d047b4e20f2941c990c0ac3f118899a9
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-06-02 12:07:59 +0530

    added a few input validation errors

--- pyclipdrop/exceptions.py
+++ pyclipdrop/exceptions.py
@@ -18,3 +18,24 @@ class FileWriteError(Exception):
     Exception raised for errors in writing to a file.
     """
     pass
+
+
+class InputValidationError(Exception):
+    """
+    Exception raised for errors in validating the inputs passed to the various methods.
+    """
+    pass
+
+
+class ValueOutOfRangeError(InputValidationError):
+    """
+    Exception raised when an input is out of an expected range.
+    """
+    pass
+
+
+class ValueTooLongError(InputValidationError):
+    """
+    Exception raised when an input is too long.
+    """
+    pass
